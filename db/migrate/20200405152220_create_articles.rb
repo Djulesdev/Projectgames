adc5f422d92fff5fe8e9047c31e696e1a18a6823
julien laloum <djulesdev@gmail.com>
2020-04-05 17:24:50 +0200

    creation models

---
+++ db/migrate/20200405152220_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :content
+      t.string :categories
+
+      t.timestamps
+    end
+  end
+end
