e554601a14c2265dc8bd560b2076f47b62032ca2
julien laloum <djulesdev@gmail.com>
2020-04-05 17:41:16 +0200

    validates on models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  belongs_to :user
+  belongs_to :game
+  CATEGORIES = ['News', 'Patchnotes', 'Reviews']
+  validates :title, presence: true, uniqueness: true
+  validates :categories, presence: true
+  validates :categories, inclusion: {in: CATEGORIES}
+  validates :content, length: {minimum: 500}
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ApplicationRecord
+  belongs_to :user
+  has_many :articles
+  has_many :users, through: :articles
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :average_rating, presence: true
 end
